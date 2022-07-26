bc9c1486e93edb922241ba26276481a916981abc
Chek Wei Chia <chek_wei_chia@aiap.sg>
2022-07-26 21:17:34 +0800

    updated repo with SQL cheatsheet

---
+++ README.md
@@ -0,0 +1,88 @@
+# SQL Cheatsheet
+Cheatsheet created for MySQL DB system.
+
+## Keywords
+### SQL Union Operator
+- Every SELECT statement within UNION must have the same number of columns
+- The columns must also have similar data types
+- The columns in every SELECT statement must also be in the same order
+
+```sql
+SELECT column_name(s) FROM table1
+UNION
+SELECT column_name(s) FROM table2;
+```
+
+### Case
+- The CASE statement goes through conditions and returns a value when the first condition is met (like an if-then-else statement).
+- So, once a condition is **true**, it will **stop reading and return the result**. 
+- If no conditions are true, it returns the value in the ELSE clause.
+- If there is no ELSE part and no conditions are true, it returns NULL.
+
+```sql
+SELECT OrderID, Quantity,
+CASE
+    WHEN Quantity > 30 THEN 'The quantity is greater than 30'
+    WHEN Quantity = 30 THEN 'The quantity is 30'
+    ELSE 'The quantity is under 30'
+END AS QuantityText
+FROM OrderDetails;
+```
+
+Sample output:
+| OrderID | Quantity | QuantityText             |
+| ------- | -------- | ------------------------ |
+| 10248   | 12       | The quantity is under 30 |
+| 10248   | 10       | The quantity is under 30 |
+| 10248   | 5        | The quantity is under 30 |
+
+
+## String Functions
+### LENGTH()
+```sql
+-- returns the length of a string (in bytes).
+SELECT LENGTH("SQL Tutorial") AS LengthOfString;
+-- output: 12
+```
+
+### LEFT()
+```sql
+-- extracts a number of characters from a string (starting from left)
+SELECT LEFT("SQL Tutorial", 3) AS ExtractString;
+-- output: SQL
+```
+
+### RIGHT()
+```sql
+-- extracts a number of characters from a string (starting from right)
+SELECT RIGHT("SQL Tutorial is cool", 4) AS ExtractString;
+-- output: cool
+```
+
+## Numeric Functions
+### LOWER(), UPPER()
+```sql
+-- converts a string to lowercase/uppercase.
+SELECT LOWER("SQL Tutorial is FUN!");
+```
+
+### CEILING()
+```sql
+-- round UP to the nearest integer
+SELECT CEILING(25.75);
+-- output: 26
+```
+
+### FLOOR()
+```sql
+-- round DOWN to the nearest integer
+SELECT FLOOR(25.75);
+-- output: 25
+```
+
+### ROUND()
+```sql
+-- rounds a number to a specified number of decimal places.
+SELECT ROUND(135.375, 2);
+-- output: 135.38
+```
--- sql_cheatsheet.sql
+++ sql_cheatsheet.sql
@@ -10,6 +10,15 @@ ORDER BY [select columns/aggregate to order by (ASC OR DESC)]
 SELECT TOP N
 FROM [table]
 
+-- get table where value is even number, exclude duplicates
+SELECT DISTINCT(CITY) FROM STATION
+WHERE ID % 2 = 0
+
+-- get names, but output names in order by the last three chars of each name, followed by order by ID asc
+SELECT NAME FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3), ID ASC
+
 -- inner join tables, get average of a column and round down to nearest int
 SELECT CO.CONTINENT, FLOOR(AVG(CI.POPULATION))
 FROM CITY CI, COUNTRY CO
@@ -30,9 +39,71 @@ WHERE LAT_N = (SELECT MIN(LAT_N)
 FROM STATION
 WHERE LAT_N > 38.7780)
 
---
-SELECT (MONTHS*SALARY) AS EARNINGS, COUNT(NAME)
+-- finding out the max value, and then get total counts of records with such max value
+-- Step 1: find the maximum value
+SELECT MAX((MONTHS * SALARY)
+
+-- Step 2a: use the max value as target for the WHERE condition
+-- Step 2b: group by the earnings, because we need to COUNT(*)
+SELECT (MONTHS*SALARY) AS EARNINGS, COUNT(*)
 FROM EMPLOYEE
 WHERE (MONTHS*SALARY) = (SELECT MAX((MONTHS * SALARY))
 FROM EMPLOYEE)
 GROUP BY EARNINGS
+
+-- using CASE in SQL
+-- overall structure
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    WHEN conditionN THEN resultN
+    ELSE result
+END;
+
+-- using CASE to create a column that says either qty is greater/is 30
+-- statement will return 3 columns [OrderID, Quantity, QuantityText]
+SELECT OrderID, Quantity,
+CASE
+    WHEN Quantity > 30 THEN 'The quantity is greater than 30'
+    WHEN Quantity = 30 THEN 'The quantity is 30'
+    ELSE 'The quantity is under 30'
+END AS QuantityText
+FROM OrderDetails;
+
+-- using CASE to figure out what type of triangle
+-- statement will return 1 column [typeOfTriangle]
+select
+case
+    when ((a+b) <= c) or ((a+c) <= b) or ((b+c) <= a) then "Not A Triangle"
+    when ((a <> b) and (b <> c) and (a <> c)) then "Scalene"
+    when ((a = b) and (a <> c)) then "Isosceles"
+    when ((a = c) and (a <> b)) then "Isosceles"
+    when ((a = b) and (b = c) and (a = c)) then "Equilateral"
+end as typeOfTriangle
+from TRIANGLES
+
+
+-- get longest and shorters value, and their respective length
+-- if there's a tie, choose the first one when ordered alphabetically
+-- step 1: get the longest 
+-- step 2: get the shortest
+-- step 3: do a union
+(SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY
+LENGTH(CITY), CITY
+LIMIT 1)
+union
+(SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY
+LENGTH(CITY) DESC, CITY
+LIMIT 1)
+
+-- query results where first and last char is a specified char
+-- step 1: use left(str, 1)
+-- step 2: use right(str, 1)
+-- step 3: use IN keyword  
+-- step 4: use AND keyword
+SELECT DISTINCT(CITY) FROM STATION
+WHERE LOWER(LEFT(CITY, 1)) IN ("a", "e", "i", "o", "u")
+AND 
+LOWER(RIGHT(CITY, 1)) IN ("a", "e", "i", "o", "u")
